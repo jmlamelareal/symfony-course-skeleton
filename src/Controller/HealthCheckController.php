8e11f6a856e79e6f77e74057c6b4849cecd8867e
José María Lamela <jmlamelareal@gmail.com>
2023-10-29 08:25:28 +0100

    feat: add log to controller

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -7,12 +7,21 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class HealthCheckController
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     #[Route('/health-check', name: 'health_check', methods: ['GET'])]
     public function __invoke(): Response
     {
+        $this->logger->error('This is the HealthCheckController error');
         return new JsonResponse(['status' => 'OK']);
     }
 }
